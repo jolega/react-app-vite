3d2f5595a6a3130ede4c2e71bdb319cfaa864fb0
jolega <alonso_juancho@hotmail.com>
2022-06-14 11:09:59 -0500

    crearndo el componente

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,11 +1,25 @@
 
 
+const newMessage = 'Johan Garcia !!!' ;
+const  welcome = () =>  {
+    return 'Welcome '
+}
+const object = {
+    name : 'Johan',
+    year : '32' ,
+};
+// <></> = Fragment 
 export const FirstApp = () => {
   return (
-    <div>
+    <>
      <h1>First App</h1>
-     <p>Soy un texto</p>
-    </div>
+     <h1>{ welcome () }</h1>
+     <h1>{ 1 + 1 }</h1>
+     <h1>{/* 1 + 1 this is a comment */ }</h1>
+     <code> { JSON.stringify( object )}</code>
+     <h1>{ newMessage } </h1>
+     <p>i am text</p>
+    </>
  
   )
 }
