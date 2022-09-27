7fb619e5bb1affeed44d56ae6acc5887d7c51c69
jolega <alonso_juancho@hotmail.com>
2022-09-27 16:07:58 -0500

    prueba test demo

---
+++ test/demo.test.js
@@ -0,0 +1,16 @@
+
+
+describe('Pruebas en <demoComponent />>', () =>  {
+
+test ('Esta prueba no debe de fallar',  () =>  {
+
+    // pruebas tiene 3 pasos, inicializa, estimulo,  observar el comportamiento
+
+
+    const message1  = 'Hola Mundo'
+
+    const message2 = message1.trim();
+
+    expect( message1 ).toBe
+})
+});
